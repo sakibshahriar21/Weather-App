45d051820f66b20d4d54914f6d973d0ab49ce86c
sakibcse570 <17201122@uap-bd.edu>
2021-10-31 18:16:16 +0600

    Reverse geolocation API added successfully

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apiKey": "Here goes the API key",
+    "apiKey": "Here goes API from openweathermap",
 
     fetchWeather: function(city) {
         fetch(
@@ -34,6 +34,67 @@ let weather = {
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+let geocode = {
+    reverseGeocode: function (latitude, longitude) {
+        var api_key = 'Here goes API from opencagedata';
+  //var latitude = '51.0';
+  //var longitude = '7.0';
+
+  var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+
+  var request_url = api_url
+    + '?'
+    + 'key=' + api_key
+    + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+    + '&pretty=1'
+    + '&no_annotations=1';
+
+  // see full list of required and optional parameters:
+  // https://opencagedata.com/api#forward
+
+  var request = new XMLHttpRequest();
+  request.open('GET', request_url, true);
+
+  request.onload = function() {
+    // see full list of possible response codes:
+    // https://opencagedata.com/api#codes
+
+    if (request.status === 200){ 
+      // Success!
+      var data = JSON.parse(request.responseText);
+      //console.log(data.results[0]); // print the location
+      weather.fetchWeather(data.results[0].components.city);
+    } else if (request.status <= 500){ 
+      // We reached our target server, but it returned an error
+                           
+      console.log("unable to geocode! Response code: " + request.status);
+      var data = JSON.parse(request.responseText);
+      console.log('error msg: ' + data.status.message);
+    } else {
+      console.log("server error");
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+    console.log("unable to connect to server");        
+  };
+
+  request.send();  // make the request
+    },
+    getLocation: function() {
+        function success (data) {
+            geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+        }
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, console.error);
+        }
+        else{
+            weather.fetchWeather()
+        }
     }
 };
 
@@ -47,4 +108,7 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
     }
 });
 
-weather.fetchWeather("Dhaka");
+//weather.fetchWeather("Dhaka");
+geocode.getLocation();
+
+
